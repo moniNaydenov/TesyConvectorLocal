11e66a6259ab3ebe5c4f6d45a91c5ce15674815a
Alexander Petrov <alexander.petrov@wacom.com>
2024-12-19 11:19:29 +0200

    Minor fix

--- custom_components/tesy_convector_local/__init__.py
+++ custom_components/tesy_convector_local/__init__.py
@@ -12,7 +12,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Tesy Convector from a config entry."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "climate")
+        hass.config_entries.async_forward_entry_setups(entry, ["climate"])
     )
     return True
 
